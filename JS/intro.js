bfdb60341be8ffc113f1d1a6dc852d0f230716f7
jake20120817 <102791234+jake20120817@users.noreply.github.com>
2022-04-29 17:00:06 +0900

    웹프로그래밍 5주차 실습

--- JS/intro.js
+++ JS/intro.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector("#login-form")
+const idInput = document.querySelector("#login-form #id")
+const passwordInput = document.querySelector("#login-form #password")
+const button = document.querySelector("#button")
 
+function onLoginButtonClick(event) {
+    event.preventDefault()
+
+    const id = idInput.value
+    const password = passwordInput.value
+
+    console.log(id)
+    console.log(password)
+
+    if (id === "jake081717J" && password === "081717J") {
+        alert("로그인 완료")
+        window.location.href = "j_illustrate.html"
+    } else {
+        alert("로그인 실패")
+    }
+}
+
+button.addEventListener("click", onLoginButtonClick)
