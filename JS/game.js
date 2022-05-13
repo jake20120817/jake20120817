ec47b036af1229e660ffa4fd59c9fdf4d8ddc7a0
jake20120817 <102791234+jake20120817@users.noreply.github.com>
2022-05-13 16:55:25 +0900

    웹 프로그래밍 게임 만들기

---
+++ JS/game.js
@@ -0,0 +1,40 @@
+var board=[
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+const ME = 1
+const AI = -1
+
+function evalute() {
+    dd
+}
+
+function gameOver() {
+    dd
+}
+
+function gameOverAll() {
+    dd
+}
+
+function emptyCells() {
+    dd
+}
+
+function setMove() {
+    dd
+}
+
+function minmax() {
+    dd
+}
+
+function aiTurn() {
+    dd
+}
+
+function clickedCell() {
+    dd
+}
