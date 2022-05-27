b20a613c988c3b4ede0ce0f6188255e751dcc10b
jake20120817 <102791234+jake20120817@users.noreply.github.com>
2022-05-27 16:59:54 +0900

    웹프로그래밍 9주차 실습

--- JS/game.js
+++ JS/game.js
@@ -11,24 +11,104 @@ function evalute() {
     dd
 }
 
-function gameOver() {
-    dd
+function gameOver(state, player) {
+    var win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[0][2], state[1][1], state[2][0]]
+    ];
+
+    for (var i = 0; i < 8; i++) {
+        var line = win_state[i]
+        var count = 0
+        for (var j = 0; j < 3; i++) {
+            if (line[j] == player) {
+                count++;
+            }
+        }
+
+        if (count == 3) {
+            return true;
+        }
+    }
+
+    return false;
+
 }
 
 function gameOverAll() {
     dd
 }
 
-function emptyCells() {
-    dd
+function emptyCells(state) {
+    var cells = []
+
+    for(var x = 0; x < 3; x++) {
+        for (var y = 0; y < 3; y++) {
+            if (state[x][y] == 0) {
+                cells.push([x][y])
+            }
+        }
+    }
+
+    return cells;
 }
 
-function setMove() {
-    dd
+function validMove() {
+    var empties = emptyCells(board)
+
+    try {
+        if (board[x][y] == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
 }
 
-function minmax() {
-    dd
+function setMove(x, y, player) {
+    if (validMove[x][y]) {
+        board[x][y] = player
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function minmax(state, depth, player) {
+    var best
+    
+    if (player == AI) {
+        best = [-1, -1, -1000]
+    } else {
+        best = [-1, -1, +1000]
+    }
+
+    if (depth == 0 || gameOverAll(state)) {
+        var score = evalute(state)
+        return [-1, -1, score]
+    }
+
+    emptyCells(state).forEach(function(cell) {
+        var x = cell[0]
+        var y = cell[1]
+        state[x][y] = player
+        var score = minmax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0] = x
+        score[1] = y
+
+        if (player == AI) {
+            
+        }
+    })
 }
 
 function aiTurn() {
